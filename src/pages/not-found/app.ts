40308f0cbee378d311a42af7d17d91c088734e01
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2023-07-26 09:07:53 +0100

    Custom not found page

---
+++ src/pages/not-found/app.ts
@@ -0,0 +1,9 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
+import {renderAsHtmlResponse} from './common/templating';
+
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const response = renderAsHtmlResponse(event, 'template.njk');
+    response.statusCode = 404;
+    return response
+};
