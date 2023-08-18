3df8c8ab7cf093f3af37d1a40f8944b7334c3349
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2023-08-18 17:04:14 +0100

    RAD-344: Fix no-session status code

--- src/pages/no-session/app.ts
+++ src/pages/no-session/app.ts
@@ -2,7 +2,5 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { renderAsHtmlResponse } from './common/templating';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const response = renderAsHtmlResponse(event, 'no-session.njk');
-    response.statusCode = 404;
-    return response;
+    return renderAsHtmlResponse(event, 'no-session.njk');
 };
