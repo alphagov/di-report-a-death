322e036820888d2feed7c7b603489755f11dbc27
Michael Willis <mike.willis@digital.cabinet-office.gov.uk>
2023-08-31 15:37:01 +0100

    RAD-392 Add hidden text

--- src/common/answer.ts
+++ src/common/answer.ts
@@ -4,9 +4,10 @@ export function includes<T extends U, U>(coll: ReadonlyArray<T>, el: U): el is T
     return coll.includes(el as T);
 }
 
-export type Answer = Partial<WhereDoYouLive & TellCivilServicePension>;
+export type Answer = Partial<WhereDoYouLive & Nino & PensionProviders & TellCivilServicePension>;
 
 export type WhereDoYouLive = { 'where-do-you-live': 'england' | 'scotland' | 'wales' | 'northern-ireland' };
-
 export type TellCivilServicePension = { 'tell-civil-service-pension': 'yes' | 'no'};
+export type PensionProviders = { 'pension-providers': 'croydon' | 'sutton'};
+export type Nino = { 'nino': string};
 
