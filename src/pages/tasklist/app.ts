94f521813c2be13b15ebfa638ebecac813afb406
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2023-08-31 11:49:12 +0100

    RAD-399: Add blank tasklist page

---
+++ src/pages/tasklist/app.ts
@@ -0,0 +1,23 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { renderAsHtmlResponse } from './common/templating';
+import { getSession } from './common/session';
+import { withErrorHandling } from './common/routing';
+
+export const lambdaHandler = withErrorHandling(async (event) => {
+    const method = event.httpMethod.toUpperCase();
+    if (method === 'GET') {
+        return get(event);
+    } else {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({
+                message: 'Method not allowed: ' + method,
+            }),
+        };
+    }
+});
+
+const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const session = await getSession(event);
+    return renderAsHtmlResponse(event, 'template.njk', { session });
+};
